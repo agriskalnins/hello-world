6c96a85468f746b3c2a44ef75b237aaedc5323d6
ZRKAC Agris Kalniņš <agriss.kalnins@gmail.com>
2019-07-05 11:26:01 +0300

    Update event.php

--- event.php
+++ event.php
@@ -139,6 +139,7 @@ echo "
         </div></a>
         <div class='social_div'>
              <a class=social_draugiem href=http://www.draugiem.lv/zrkac>&nbsp;</a>
+
              <!--<a class=social_facebook href=#>&nbsp;</a>-->
         </div>
         <div class='divClear'>
